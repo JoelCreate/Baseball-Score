e26ac0911a7ced2431d9097a8be48199b0057ace
Joel Wiesner <j.wiesner88@gmail.com>
2023-06-06 19:09:16 -0700

    working on slide for the score

--- app.js
+++ app.js
@@ -2,3 +2,18 @@ function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }  
 
+const slider = document.querySelector('.slider')
+const sliderWrapper = document.querySelector('.slider-wrapper')
+const pagination = document.querySelectorAll('.pagination span')
+let slideId = 1
+
+function slide(id){
+   sliderWrapper.getElementsByClassName.left = -100 * id + "%" 
+
+    pagination.foeEach(page => {
+        page.classList.remove('active')
+    })
+
+    pagination[id].classList.add('active')
+
+}
