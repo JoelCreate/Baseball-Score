ddad276388f5df0da28504f398819c787efd9e2a
Joel Wiesner <j.wiesner88@gmail.com>
2023-06-07 18:58:27 -0700

    made a second modal window for the away team name and added animation to css

--- app.js
+++ app.js
@@ -2,18 +2,36 @@ function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }  
 
-const slider = document.querySelector('.slider')
-const sliderWrapper = document.querySelector('.slider-wrapper')
-const pagination = document.querySelectorAll('.pagination span')
-let slideId = 1
+const modalHome = document.getElementById('modal-1')
+const modalAway = document.getElementById('modal-2')
+const homeTeam = document.getElementById('home-team-el')
+const submitBtnHome = document.getElementById('btn-el')
+const homeTeamInput = document.getElementById('home-team-input')
+const homeTeamName = document.getElementById('home-team-el')
 
-function slide(id){
-   sliderWrapper.getElementsByClassName.left = -100 * id + "%" 
 
-    pagination.foeEach(page => {
-        page.classList.remove('active')
-    })
+submitBtnHome.addEventListener("click", function(){
+    let homeTeamEntered = homeTeamInput.value
 
-    pagination[id].classList.add('active')
+    modalHome.style.display = 'none'
+    modalAway.style.display = 'block'
+    homeTeamName.textContent = homeTeamEntered
+})
 
-}
+
+
+// const slider = document.querySelector('.slider')
+// const sliderWrapper = document.querySelector('.slider-wrapper')
+// const pagination = document.querySelectorAll('.pagination span')
+// let slideId = 1
+
+// function slide(id){
+//    sliderWrapper.getElementsByClassName.left = -100 * id + "%" 
+
+//     pagination.foeEach(page => {
+//         page.classList.remove('active')
+//     })
+
+//     pagination[id].classList.add('active')
+
+// }
