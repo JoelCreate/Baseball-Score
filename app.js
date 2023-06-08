1538f93438d951adcc36f1514250b2343b74be14
Joel Wiesner <j.wiesner88@gmail.com>
2023-06-07 19:11:07 -0700

    added functions to modal windows so team name inputted corresponds with home and away team properly

--- app.js
+++ app.js
@@ -2,22 +2,44 @@ function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }  
 
+const modalWindow = document.getElementById('modal-window')
 const modalHome = document.getElementById('modal-1')
 const modalAway = document.getElementById('modal-2')
-const homeTeam = document.getElementById('home-team-el')
 const submitBtnHome = document.getElementById('btn-el')
+const submitBtnStart = document.getElementById('btn-el-start')
 const homeTeamInput = document.getElementById('home-team-input')
 const homeTeamName = document.getElementById('home-team-el')
+const awayTeamInput = document.getElementById('away-team-input')
+const awayTeamName = document.getElementById('away-team-el')
+
 
 
 submitBtnHome.addEventListener("click", function(){
-    let homeTeamEntered = homeTeamInput.value
+    let homeTeamEntered = homeTeamInput.value    
 
-    modalHome.style.display = 'none'
-    modalAway.style.display = 'block'
-    homeTeamName.textContent = homeTeamEntered
+    if (homeTeamInput.value === "") {
+        alert('Enter a name for the home team')
+    }
+    else{
+        homeTeamName.textContent = homeTeamEntered
+        modalHome.style.display = 'none'
+        modalAway.style.display = 'block'
+    }
+    
 })
 
+submitBtnStart.addEventListener("click", function() {
+    let awayTeamEntered = awayTeamInput.value
+
+    if (awayTeamInput.value === "") {
+        alert('Please enter a name for the away team')
+    }
+    else {
+        awayTeamName.textContent = awayTeamEntered
+        modalWindow.style.display = 'none'
+    }
+    
+})
 
 
 // const slider = document.querySelector('.slider')
